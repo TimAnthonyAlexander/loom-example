728c6cd1e04b1bb5c9caf8ebc8edf88c99503184
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-07-22 01:34:04 +0200

    More files.... its still working2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
